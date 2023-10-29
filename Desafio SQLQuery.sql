855a6f2e952b021bde88de90b029f9dfd6991b2b
eddieavlis <146478309+eddieavlis@users.noreply.github.com>
2023-10-29 01:20:28 -0300

    Update Desafio SQLQuery.sql

--- Desafio SQLQuery.sql
+++ Desafio SQLQuery.sql
@@ -1,1 +1,67 @@
-l
+-- Desafios de Projeto: Montando consultas relacionais no SQL Server
+
+-- 1
+SELECT Nome, Ano
+FROM Filmes;
+
+-- 2
+SELECT Nome, Ano, Duracao
+FROM Filmes
+ORDER BY Ano ASC;
+
+-- 3
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro';
+
+-- 4
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Ano = 1997;
+
+-- 5
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Ano > 2000;
+
+-- 6
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao ASC;
+
+-- 7
+SELECT Ano, COUNT(*) AS QuantidadeDeFilmes
+FROM Filmes
+GROUP BY Ano
+ORDER BY COUNT(*) DESC;
+
+-- 8
+SELECT Id, PrimeiroNome, UltimoNome, Genero
+FROM Atores
+WHERE Genero = 'M';
+
+-- 9
+SELECT Id, PrimeiroNome, UltimoNome, Genero
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome;
+
+-- 10
+SELECT F.Nome AS NomeDoFilme, G.Genero
+FROM Filmes F
+JOIN FilmesGenero FG ON F.Id = FG.IdFilme
+JOIN Generos G ON FG.IdGenero = G.Id;
+
+-- 11
+SELECT F.Nome AS NomeDoFilme, G.Genero
+FROM Filmes F
+JOIN FilmesGenero FG ON F.Id = FG.IdFilme
+JOIN Generos G ON FG.IdGenero = G.Id
+WHERE G.Genero = 'Mistério';
+
+-- 12
+SELECT F.Nome AS NomeDoFilme, A.PrimeiroNome, A.UltimoNome, EF.Papel
+FROM Filmes F
+JOIN ElencoFilme EF ON F.Id = EF.IdFilme
+JOIN Atores A ON EF.IdAtor = A.Id;
